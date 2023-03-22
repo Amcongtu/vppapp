2777efaf788f7e8931dde9bea5edc0c129280342
lebaonhi12c1 <lebaonhi12c1@gmail.com>
2023-03-22 10:59:01 +0700

    Up footer

--- src/components/home/PopularSearch.js
+++ src/components/home/PopularSearch.js
@@ -4,7 +4,7 @@ import { BsSearch } from 'react-icons/bs';
 
 function PopularSearch(props) {
     return (
-        <div>
+        <div>  
             <div className="container_main">
                 <div className=" my-[10px] bg-white rounded-md ">
                     <div className='flex pt-[20px] pb-[15px] px-[20px] gap-2 '>
@@ -15,23 +15,23 @@ function PopularSearch(props) {
                     </div>
                     <div className="grid grid-cols-5 ">
                         <div className="col-span-1 border-[1px] border-gray-200 h-[112px] flex justify-between items-center px-2 hover:cursor-pointer select-none">
-                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2">Giấy Định Lượng 120</span>
+                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2 pl-2">Giấy Định Lượng 120</span>
                             <div className=''><BsSearch className=" text-[28px] mr-3"/></div>
                         </div>
                         <div className="col-span-1 border-[1px] border-gray-200 h-[112px] flex justify-between items-center px-2 hover:cursor-pointer select-none">
-                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2">Milo Nắp Vặn 200ml</span>
+                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2 pl-2">Milo Nắp Vặn 200ml</span>
                             <div className=''><BsSearch className=" text-[28px] mr-3"/></div>
                         </div>
                         <div className="col-span-1 border-[1px] border-gray-200 h-[112px] flex justify-between items-center px-2 hover:cursor-pointer select-none">
-                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2">Con Dấu S830</span>
+                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2 pl-2">Con Dấu S830</span>
                             <div className=''><BsSearch className=" text-[28px] mr-3"/></div>
                         </div>
                         <div className="col-span-1 border-[1px] border-gray-200 h-[112px] flex justify-between items-center px-2 hover:cursor-pointer select-none">
-                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2">Double A A4 70gsm</span>
+                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2 pl-2">Double A A4 70gsm</span>
                             <div className=''><BsSearch className=" text-[28px] mr-3"/></div>
                         </div>
                         <div className="col-span-1 border-[1px] border-gray-200 h-[112px] flex justify-between items-center px-2 hover:cursor-pointer select-none">
-                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2">Giá Giấy A4 Double A 80gsm</span>
+                            <span className="block font-[500] text-[14px] text-[#343434] capitalize line-clamp-2 pl-2">Giá Giấy A4 Double A 80gsm</span>
                             <div className=''><BsSearch className=" text-[28px] mr-3"/></div>
                         </div>
                     </div>
--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -3,17 +3,197 @@ import React from 'react';
 function Footer(props) {
     return (
         <div>
-            <div className="container_main">
-                <div>Footer</div>
-                <div className="grid grid-cols-10 h-[30px]">
-                    <div className=" col-span-2">asdasdas</div>
-                    <div className=" col-span-2">asdasdas</div>
-                    <div className=" col-span-2">asdasdas</div>
-                    <div className=" col-span-2">asdasdas</div>
-                    <div className=" col-span-2">asdasdas</div>
-                    <div className=" col-span-2">asdasdas</div>
+            <div className="mt-[20px] py-[12px] border-main-bg border-b-0 border-x-0 border-t-[4px]">
+                <div className="container_main pb-[200px] text-gray-600">
+                    <div className='font-[600] text-[16.75px] py-[5px]'>Danh mục sản phẩm</div>
+                    <div className="grid grid-cols-10 h-[30px] gap-6">
+                        <div className=" col-span-2  ">
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Giấy in Ấn - Photo</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Giấy in văn phòng - Giấy photo chất lượng / Giấy in liên tục -In bill -Fax nhiệt / Giấy note - Giấy phân trang / Decal đế xanh - Decal đế vàng -Tomy / Giấy than - Giấy kẽ ngang - Giấy Roky / Giấy FO màu</span>
+                                </div>
+                            </div>
+                           <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Bìa - Kệ - Rổ</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bìa lá -trình ký -Cardcase / Bìa lỗ -Phân trang -Bìa lò xo / Rổ xéo -Kệ nhựa -Kệ mica / Bìa nút -Cặp 12 ngăn -Bìa kẹp / Bìa treo -Bìa cây -Bìa accor / Bìa dây -Bìa hộp / Bìa nhiều lá nhựa - da / Bìa thái / Bìa kiếng / Bìa Còng</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Sổ - Tập - Bao Thư</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Sổ da đen - Sổ lò xo - Sổ caro / Tập vở - Bao thư / Sổ Namecard - Hộp đựng Namecard / Phiếu Thu Chi - Phiếu Nhập Xuất Kho</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Bút - Mực Chất Lượng Cao</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bút bi - Bút nước - Bút ký / Bút dạ quang đầy đủ màu sắc, chất lượng / Bút xóa - Băng xóa - Ruột xóa / Bút chì -Ruột chì -Tẩy -Chuốt- Giá tốt / Mực dấu -Lông bảng -Lông dầu / Bút bi-bút nước Thiên Long / Bút lông bảng / Bút lông dầu đa dạng, phong phú / Hộp cắm bút</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Dụng Cụ Văn Phòng Chất Lượng</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bấm kim -Kim bấm -Kẹp giấy / Kẹp bướm -Kẹp acco -Gỡ ghim / Máy bấm kim -Bấm lỗ các loại / Bảng tên - dây đeo / Tủ hồ sơ - kính lúp / Ép Plastic A3,A4,A5,CMND,bằng lái / Máy bấm kim đại -kim bấm / Máy bấm gỗ -kim bấm gỗ / Bấm kim trung(03) -kim bấm</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Băng Keo - Dao - Kéo</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Băng keo trong/đục -Simili / Cắt băng keo / Dao rọc giấy - lưỡi dao / Súng bắn keo -Bắn giá -Keo silicon / Băng keo 2 mặt -Giấy -Xốp / Hồ nước -Hồ khô / Kéo các loại / Bàn cắt giấy A4 -A3 / Thước các loại</span>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className=" col-span-2  ">
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Máy Tính Casio</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Máy Tính Để Bàn / Máy Tính Có Chức Năng in Giấy / Máy Tính Sắc Màu / Máy Tính Bỏ Túi</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Bách Hóa Online</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Tạp hóa văn phòng / Nước uống các loại / Cà Phê / Trà / Bánh kẹo / Mì -Miến -Cháo -Phở / Đồ uống các loại / Sữa các loại / Chăm sóc cho bé / Mì, Cháo, Phở ăn liền / Dầu ăn, nước chấm, gia vị / Bánh kẹo các loại / Chăm sóc cá nhân / Vệ sinh nhà cửa / Đồ dùng gia đình / Gạo, thực phẩm khác / Đồ đông lạnh, đồ mát</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Bảng Văn Phòng</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bảng viết bút lông cao cấp / Bảng ghim - Bảng lịch công tác - Bảng viết phấn / Bảng di động / Bảng Flipchart / Mút lau bảng,Nam châm,Phấn</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Dịch Vụ Khắc Dấu Uy Tín</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Con dấu tên - Chức danh / Máy đóng số xoay -Số nhảy / Tampon dấu - Caosu mặt dấu chất lượng tốt / Bảng tên CTY-Số nhà-Phòng ban / Dấu chữ ký -Bút ký có dấu / Dấu ngày..tháng..năm - tự động / Dấu tròn / Dấu vuông thông dụng / Tampon- Con dấu- Mực dấu Shiny</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Bảo Hộ Lao Động</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Thiết bị, dụng cụ PCCC / Giày, ủng bảo hộ lao động / Găng tay bảo hộ / Khẩu trang, mặt nạ / Kính bảo hộ / Dây đai an toàn / Nón bảo hộ và phụ kiện / Quần áo bảo hộ / Vật tư phòng sạch / Thiết bị chống ồn / Sản phẩm bảo hộ khác / Mặt nạ hàn / An toàn giao thông, công trình / Kiếng hàn</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Nhà cửa và Đời Sống</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Đồ gia dụng -Linh tinh / Ấm Bình Ly / Dụng cụ tập gym -Massage / Dụng cụ sửa chữa / Dụng cụ đi bơi -đi mưa / Dụng cụ y tế / Đồng hồ / Đồ trang trí -Quà tặng / Gậy chụp hình / Hột quẹt / Khăn - võng / Mùng -Mền -Gối / Móc khóa -Ổ khóa / Phụ kiện nhà bếp / Phụ kiện đồ chơi xe / Thiết bị vệ sinh / Trang trí noel</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" col-span-2  ">
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Sản phẩm văn phòng phẩm khác</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Văn phòng phẩm khác / Phòng cháy chữa cháy / Bút / Dấu đóng / Văn Phòng Phẩm Văn Phòng Phong Phú, Đa Dạng / Thiết bị văn phòng phẩm / Vật phẩm quảng cáo / Sản phẩm 3M</span>
+                                </div>
+                            </div>
+                           <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Pin Các Loại</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Pin dự phòng / Pin điện thoại - Pin các loại / Pin Energizer / Pin Sony / Pin Camelion / Pin Panasonic / Pin Duracell / Pin Maxell / Pin Fujitsu / Pin Mitsubishi / Pin Tcbest / Pin AA – Pin tiểu / Pin AAA – Pin đũa / Pin 3V – Pin cúc áo / Pin Cr2-3V / Pin Cr123-3V / Pin 9V – Pin vuông / Pin 12V – Pin điều khiển / Pin đồng hồ / Pin trung – Pin C / Pin đại – Pin D / Pin Máy Trợ Thính / Pin sạc AA / Pin sạc AAA / Máy sạc pin / Sạc pin AA / Sạc pin AAA / Sạc pin 9V / Pin nuôi nguồn PLC – CNC 3.6v Lithium / Pin nuôi nguồn PLC – CNC 3v Lithium / Pin nuôi nguồn PLC – CNC 6V Lithium / Pin Sạc 18650 3.7V / Pin Sạc 3.7v Li-Polymer / Pin Pkcell / Pin Điện Thoại Iphone / Pin Robot hút bụi - Máy hút bụi / Pin Pisen / Pin Ansmann / Pin Toshiba / Pin GP / Pin Rocket / Pin Máy Ảnh / Sạc Máy Ảnh / Pin Renata / Pin Mavic Air FLycam / Wifi 3G/4G Kèm Pin / Pin Sạc Dự Phòng</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Giấy In Ảnh - In Màu Chất Lượng Cao</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Giấy ảnh / Giấy in màu</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Chăm Sóc Nhà Cửa</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Chất tẩy rửa -Vật phẩm vệ sinh / Thùng rác -Dụng cụ vệ sinh</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Điện Gia Dụng</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Ổ cắm điện -Phích cắm / Bóng đèn -Đèn ngủ / Đèn để bàn -Đèn bắt muỗi / Đèn pin - Đèn đội đầu / Quạt làm mát / Thiết bị điện</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Điện Thoại Và Phụ Kiện</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bộ sạc - cóc - cáp / điện thoại / Miếng dán cường lực / Màn hình - ốp lưng / Phụ kiện điện thoại / Thẻ nhớ - usb - đầu đọc thẻ / Tai nghe cắm dây / Tai bluetooth</span>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className=" col-span-2  ">
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Thiết Bị Văn Phòng</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Pin -CD -DVD -USB -Điện thoại bàn / Máy đóng gáy sách -Lò xo / Máy hủy tài liệu -Máy đếm tiền / Phụ kiện máy tính Chuột -Bàn phím / Xe đẩy hàng / Bàn phím máy tính / Chuột máy tính / Phụ kiện máy tính</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Thiết Bị Điện Tử</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bộ đàm - máy ghi âm / Camera quan sát - định vị / Loa bluetooth / Loa kẹo kéo - loa karaoke / Loa vi tính / Loa nghe pháp - fm - mp3 / Mic karaoke - card âm thanh / Thiết bị mạng - wifi / Thiết bị truyền hình - tivi / Thiết Bị Camera An Ninh</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Mẹ Và Bé</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Đồ chơi trẻ em / Xe điện - xe đẩy cho bé</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Sức Khoẻ Và Sắc Đẹp</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Chăm sóc sức khỏe và sắc đẹp / Mỹ phẩm - làm đẹp / Nước hoa / Trang sức - nữ trang</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Đồ Dùng Học Sinh</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span></span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Sản Phẩm Ngành Nhựa</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Hộp Thực Phẩm Happi Lock / Xô Nhựa / Bàn Ghế Nhựa / Kệ nhựa / Giỏ nhựa / Bình ca tách / Thau nhựa / Đĩa nhựa / Ly chén Tô nhựa 2 màu / Thùng nhựa / Mâm Úp ly Gáo nước / Thố Hộp cơm / Sọt Sóng Cần xé nhựa / Ống đũa Móc áo Vỉ đá / Khay Vỉ nhựa / Bình đá / Hàng gia công</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" col-span-2  ">
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Sản Phẩm Thiên Long</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Bút viết / Dụng cụ văn phòng office / File Bìa hồ sơ / Băng keo - hồ dán / Chăm sóc cá nhân office / Quà tặng - Bút bi / Quà tặng - Bút máy / Quà tặng - Bút lông bi / Tô màu / Ba lô / Phụ kiện học sinh / TẬP TÔ MÀU - TÔ CHỮ / SÁP NẶN / TẬP HỌC SINH / BỘ DỤNG CỤ HỌC TẬP / THƯỚC - COMPA / KÉO HỌC SINH / GIẤY KIỂM TRA -NHÃN VỞ / CHUỐT BÚT CHÌ / BẢNG HỌC SINH / GÔM / Máy in văn phòng / Máy in công nghiệp / Nhãn in</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Mực In - Bơm Mực</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span>Mực in HP / Mực in Canon / Mực in Samsung / Mực in Brother / Ruy băng - Film fax / Mực nạp máy in / Mực gói photocopy / Mực in phun / Hộp mực máy in / Mực hộp photocopy / Linh kiện hộp mực in / Linh kiện máy in / Linh kiện photocopy</span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Ăn Vặt Văn Phòng</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span></span>
+                                </div>
+                            </div>
+                            <div className='mb-[12px]'>
+                                <span className='text-[14.75px] font-[500]  py-[2px]'>Quà Tặng Doanh Nghiệp</span>
+                                <div className='text-[13.5px] font-[400] py-[2px] cursor-pointer select-none'>
+                                    <span></span>
+                                </div>
+                            </div>
+
+                        </div>
+                      
+                      
+                    </div>
                 </div>
             </div>
+           
         </div>
     );
 }
